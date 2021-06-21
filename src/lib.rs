6e5f93f44633b8f6a2db9ed1d007ac6b8280b883
amiya <42173775+amiyamandal-dev@users.noreply.github.com>
2021-06-21 08:52:53 +0530

    update indent

--- src/lib.rs
+++ src/lib.rs
@@ -19,14 +19,14 @@ mod tests {
     #[test]
     fn test_search_for_value_binary_search() {
         let v = (0..100000).filter(|&i| i % 5 != 0).collect::<Vec<usize>>();
-        let rez = sort::search::binary_search(&v, 100000, 0_usize, v.clone().len()-1_usize);
+        let rez = sort::search::binary_search(&v, 100000, 0_usize, v.clone().len() - 1_usize);
         assert_eq!(Ok(6400 as usize), rez);
     }
 
     #[test]
     fn test_search_for_value_binary_search_fail_case() {
         let v = (0..100000).filter(|&i| i % 5 != 0).collect::<Vec<usize>>();
-        let rez = sort::search::binary_search(&v, 100001, 0_usize, v.clone().len()-1_usize);
+        let rez = sort::search::binary_search(&v, 100001, 0_usize, v.clone().len() - 1_usize);
         assert_eq!(Err(0 as usize), rez);
     }
 }
--- src/sort/search.rs
+++ src/sort/search.rs
@@ -1,32 +1,33 @@
 pub fn linear_search<T: Ord + std::fmt::Debug>(v: &[T], i: T) -> Result<usize, usize> {
     for (p, x) in v.iter().enumerate() {
         if *x == i {
-            return Ok(p)
+            return Ok(p);
         } else if *x > i {
-            return Err(p)
+            return Err(p);
         }
     }
     Err(0)
 }
 
-pub fn binary_search<T: Ord + std::fmt::Debug>(v: &[T], i: T, start_index:usize, end_index:usize) -> Result<usize, usize> {
+pub fn binary_search<T: Ord + std::fmt::Debug>(
+    v: &[T],
+    i: T,
+    start_index: usize,
+    end_index: usize,
+) -> Result<usize, usize> {
     let mid = (start_index + end_index) / 2 as usize;
     let mid_element = &v[mid];
     if *mid_element == i {
         return Ok(mid);
     } else if *mid_element > i {
-        return binary_search(v ,i, start_index, mid -1_usize);
+        return binary_search(v, i, start_index, mid - 1_usize);
     } else if *mid_element < i {
         if start_index <= mid {
             return Err(mid);
-        }
-        else {
-            return binary_search(v, i, mid + 1_usize, end_index);   
+        } else {
+            return binary_search(v, i, mid + 1_usize, end_index);
         }
     } else {
         return Err(mid);
     }
-
-} 
-
-
+}
